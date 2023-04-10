f80896d5d2e3c76b4db6873d46cd862f4849bdc9
romeodombaj <romeodombaj@gmail.com>
2023-04-10 12:43:30 +0200

    recipe post function created

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const db = require("../db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Recipe list");
+});
+
+router.post("/", (req, res) => {
+  const recipe = req.body;
+
+  console.log(recipe);
+
+  db.collection("recipes")
+    .insertOne(recipe)
+    .then((result) => {
+      res.status(201).json(recipe);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: "Could not add new recipe" });
+    });
 });
 
 module.exports = router;
